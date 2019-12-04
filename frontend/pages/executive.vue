373ebae1c170e1706539657657aa85edec85f611
icoxfog417 <icoxfog417@yahoo.co.jp>
2019-12-04 19:34:23 +0900

    Update Docker

--- frontend/pages/executive.vue
+++ frontend/pages/executive.vue
@@ -1,17 +1,18 @@
 <template>
   <v-layout column justify-center align-center>
     <div class="text-center">
-      <div class="display-3">エグゼクティブになった!</div>
+      <div class="display-3">
+        エグゼクティブになった!
+      </div>
       <hr />
       <br />
       <v-card class="mx-auto" max-width="400">
         <v-img src="/after.png"></v-img>
         <v-card-title>
-          {{ executive.filer_name }}
+          {{ executive.filer_name }} <br />
+          (証券コード: {{ secCode }})
         </v-card-title>
-        <v-card-subtitle class="pb-0">
-          {{ executive.edinet_code }}
-        </v-card-subtitle>
+        <v-card-title>{{ salary }}円ゲット!</v-card-title>
       </v-card>
     </div>
   </v-layout>
@@ -20,8 +21,10 @@
 <script>
 export default {
   async asyncData(ctx) {
+    // 1944 is maximum number of company
+    const index = Math.floor(Math.random() * Math.floor(1944));
     return {
-      executive: await ctx.app.$executiveRepository.get(1).catch(error => {
+      executive: await ctx.app.$executiveRepository.get(index).catch(error => {
         console.log(error);
         return {
           executive: {}
@@ -33,6 +36,15 @@ export default {
     return {
       executive: {}
     };
+  },
+  computed: {
+    secCode() {
+      return this.executive.sec_code.slice(0, 4);
+    },
+    salary() {
+      const salary = Math.round(this.executive.amount / this.executive.number);
+      return salary.toLocaleString();
+    }
   },
   methods: {
     async beExecutive() {
